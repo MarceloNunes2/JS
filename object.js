67fd45c610412746747ad06cabade40514fe5e44
marcelonunes2 <mklelo1990@gmail.com>
2022-09-30 11:51:35 -0300

    objects

---
+++ object.js
@@ -0,0 +1,22 @@
+// ObJect
+// Propriedade : valor
+// Marca : Sonu
+// cor : preta
+// tamanho : 75°
+
+// Nome: Marcelo
+// Idade : 31
+// altura 1.7m
+
+const person ={
+    name: "Marcelo",
+    age: 31,
+    height : 1.7,
+    sex: "male",
+    address :{
+        street : "rua cocaia",
+        number : "20",
+        contry : "Brasil"
+    }
+}
+console.log(person.address.street)
