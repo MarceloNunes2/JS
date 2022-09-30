bce4cb8c9705e3ef4d056f967815a6e703cc8ad2
marcelonunes2 <mklelo1990@gmail.com>
2022-09-30 10:10:55 -0300

    Começando os estudos de Js

---
+++ stings.js
@@ -0,0 +1,19 @@
+/* String 
+
+- Cadeia de Caracteres 
+*/
+
+let myfristString = "Aqui escrevo o que eu quiser"
+let mySecondString = 'Escrevo Novamente'
+let myThirdString = `Com a Crase Também` //Template literals ou template strings
+
+let text = "Fulando de tal faloo o seguinte 'Aprendendo estou' "
+
+let numberOfPeople = 34
+
+let MytextCrase = `Posso colocar Variaveis aqui dentro ${numberOfPeople + 10 / 4}`
+
+let myTextWithSuperPower = `Com a crase eu consigo 
+ir pulando linha com as aspas não`
+
+console.log(MytextCrase)
