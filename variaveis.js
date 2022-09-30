29bb5962adeaec912b023ecdf0b84cd73975eb74
Marcelo Nunes <106832862+MarceloNunes2@users.noreply.github.com>
2022-09-30 19:08:13 -0300

    Add files via upload
    incremento de codigos

---
+++ variaveis.js
@@ -0,0 +1,36 @@
+// VARIAVEIS
+
+var numero1 = 1 //Não Usar o Var
+
+let numero2 =  2
+
+const numero3 = 3
+
+numero1 = 10
+numero2 = 20
+
+
+
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+//Permitidos
+
+// - Iniciar com $ ou _
+// - Colocar acentos
+// - Iniciar com letras
+// - Letras maisculas e minusculas
+
+//Não pode:
+
+// - iniciar com números
+// - colocar espaços
+
+//Ideal
+
+// - Camel case // aPrimeiraMaiuscula
+// - Snake case // separadas_por_underline
+// - Sempre em Ingles
+// - nomes que fazem sentido
