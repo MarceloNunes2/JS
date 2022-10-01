ddb72e4a618cc234cafb06caead38892635cfbb6
marcelonunes2 <mklelo1990@gmail.com>
2022-09-30 21:41:05 -0300

    estudos Js

---
+++ array.js
@@ -0,0 +1,54 @@
+/*
+
+ARRAY - Vetores ou Listas
+
+*/
+
+const number = [10,5,234,25,{
+    name: "Marcelo",
+    age: 31,
+    height : 1.7,
+    sex: "male",
+    address :{
+        street : "rua cocaia",
+        number : "20",
+        contry : "Brasil"
+    }
+},"to locão"]
+
+console.log(number)
+
+
+const people = [{
+    name: "Marcelo",
+    age: 31,
+    height : 1.7,
+    sex: "male",
+    address :{
+        street : "rua cocaia",
+        number : "20",
+        contry : "Brasil"
+    }
+},
+{
+    name: "Luca",
+    age: 24,
+    height : 1.84,
+    sex: "male",
+    address :{
+        street : "rua Land",
+        number : "45",
+        contry : "Brasil"
+    }
+},
+{
+    name: "Mande",
+    age: 19,
+    height : 1.5,
+    sex: "famale",
+    address :{
+        street : "rua cocaia",
+        number : "80",
+        contry : "Brasil"
+    }
+}]
---
+++ bolleam.js
@@ -0,0 +1,7 @@
+//Bolleam
+
+
+let myNumber = 10
+let myNumber2 = 20
+
+ console.log(myNumber == myNumber2)
