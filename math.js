50346a5107458ae2cf6ca0af5836faaa17cc0c4e
marcelonunes2 <mklelo1990@gmail.com>
2022-10-01 11:41:49 -0300

    Aulas de JS

---
+++ math.js
@@ -0,0 +1,10 @@
+/*
+MATH
+*/
+
+console.log(Math.PI) //Mostra Valor de PI
+console.log(Math.pow(2,2)) // Valores de Potência 1° Base 2° Expoente
+console.log(Math.sqrt(25,2)) //Valores de Rais Quadrada
+console.log(Math.random())//Mostra um numéro aleatorio emtre 0 e 1
+console.log(Math.ceil(24.5))//Arredondar um numéro para cima
+console.log(Math.floor(24.5)) //Arredondar um numéro para baixa
