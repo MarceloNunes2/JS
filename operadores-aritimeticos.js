c4d5bc5bc63cea2d5f3c484a01c05d96ef75f120
marcelonunes2 <mklelo1990@gmail.com>
2022-10-03 12:13:26 -0300

    mais dados

---
+++ operadores-aritimeticos.js
@@ -0,0 +1,16 @@
+/*
+    Operadores aritimeticos
+
+    + ---> soma
+    - ---> subtração
+    * ---> multiplicação
+    / ---> divisão
+    & ---> resto
+    ++ ---> incremento
+    -- ---> decrementro
+    ** ---> exponencial
+*/
+
+console.log(7%3)
+
+console.log(7/4)
---
+++ operadores-de-atribuicao.js
@@ -0,0 +1,15 @@
+/*
+    Operadores de Atribuição
+
+    = --> Atribuição
+    += --> Adição
+    -= --> Subtração
+    /= --> Divisão
+    *= --> Multiplicação
+    %= --> Resto da divisão
+*/
+
+let number = 40
+number %= 5
+
+console.log(number)
