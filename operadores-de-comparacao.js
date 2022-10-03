91340979fe7d003875e86a3454dd36bca271b2e3
marcelonunes2 <mklelo1990@gmail.com>
2022-10-03 14:55:01 -0300

    exercicios js

---
+++ operadores-de-comparacao.js
@@ -0,0 +1,25 @@
+/*
+    Operadores de comparação
+
+    == --> Igual // ele compara o valor, mas não compara o tipo do valor  // Não usar esse 
+    === --> Totalmente igual // ele verifica o tipo de dado
+    
+    != --> Diferente // Ele compara o valor, mas não compara o tipo do valor // Não usar
+    !== --> Totalmente Diferente // ele verifica o tipo de dado
+
+    > --> Maior
+    >= --> Maior igual
+    < --> Menor
+    <= --> Menor Igual
+
+*/
+
+// const fristPerson = "João"
+// const seccondPerson = "Maria"
+
+// console.log(fristPerson !== seccondPerson)
+
+const numberone = 10
+const numbertwo = 10
+
+console.log(numberone <= numbertwo)
