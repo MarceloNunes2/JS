49a8a483d421fd9e4820a6a4fda9ace2ff4e11cb
marcelonunes2 <mklelo1990@gmail.com>
2022-10-04 10:54:29 -0300

    logicos

---
+++ operadores-logicos.js
@@ -0,0 +1,18 @@
+/*
+    OPERADORES LÓGICOS
+
+    &&  --> E
+    true && true  = true
+    true  && false = false
+    false && false = false
+
+    || --> Ou 
+    true || true = true
+    true || false = true
+    false || false = false
+
+    ! --> Negação
+    !true = false 
+    !false = true
+*/
+
