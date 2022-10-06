f861b07347b19ec6ee5ce8821f5f8452497913fb
marcelonunes2 <mklelo1990@gmail.com>
2022-10-06 14:55:36 -0300

    estudos

---
+++ controlador de flixo if/else.js
@@ -0,0 +1,17 @@
+/*
+        Controladores de Fluxo If/else
+
+        if = se
+        else = senão
+
+*/
+
+const peso = 91
+
+if (peso >= 80){
+   console.log (`Acima do peso`) 
+}
+else{
+    console.log(`Peso normal`)
+}
+console.log("Esta finalizado")
---
+++ imc.js
@@ -0,0 +1,8 @@
+const dados = {
+    nome : `Marcelo`,
+    peso : 70,
+    heigth : 1.70
+}
+const res  = (dados.peso / dados.heigth)
+
+console.log(res)
