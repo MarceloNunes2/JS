7ebb8216b8e045b019a33779410186afa08d871c
marcelonunes2 <mklelo1990@gmail.com>
2022-10-20 21:14:35 -0300

    estudos

---
+++ array-fuction-2.js
@@ -0,0 +1,24 @@
+/*  
+    Array Function
+
+    Splice 
+    Slice 
+    pop
+    Shift
+*/
+
+const students = [`Marcelo`,`Maria`,`Juliana`,`Souzou`,`Lucas`,`Cometa`]
+
+// students.splice(6,0,`Santos`)
+
+// console.log(students)
+
+// const newstudets = students.slice(1,4)
+
+// console.log(newstudets)
+
+// students.pop()
+
+// students.shift()
+
+console.log(students)
---
+++ foreach.js
@@ -0,0 +1,28 @@
+/*
+    For Each
+*/
+
+const students = [
+    {Nome: `Lucas `, age:25},
+    {Nome: `Maria `, age:67},
+    {Nome: `Juliana `, age:27},
+    {Nome: `Souza `, age:57},
+    {Nome: `Marcelo `, age:32},
+    {Nome: `Cometa`, age:2},
+    {Nome: `Luna `, age:6},
+    {Nome: `Meliodas `, age:5},
+    {Nome: `Theo `, age:10},
+    {Nome: `Steve `, age:21},
+
+];      
+    let = allStrudetsAge = 0
+students.forEach((studets,index) => {        
+
+        {
+         console.log(`O aluno chama-se${studets.Nome}, ele tem ${studets.age} anos, e o numero dele na chamada é ${index}`)
+        }
+
+})
+
+// const averengeage = allStrudetsAge / students.length
+// console.log(`a Media de idade de alunos é de ${averengeage.toFixed(0)}`)
