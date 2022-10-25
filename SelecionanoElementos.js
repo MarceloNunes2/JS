4fe25286779128733000cef86a4101bc7342913e
marcelonunes2 <mklelo1990@gmail.com>
2022-10-25 14:28:36 -0300

    MODULO3JS

---
+++ SelecionanoElementos.js
@@ -0,0 +1,26 @@
+// const element = document.getElementById("main-text")
+
+// console.log(element)
+
+// const element = document.getElementsByClassName("paragraph")
+
+// console.log(element)
+
+
+// const element = document.getElementsByTagName("h1")
+
+// console.log(element)
+
+
+// const element = document.querySelector ("")
+
+// console.log(element)
+
+// const element = document.querySelectorAll('p')
+
+// element.forEach(element =>{
+//     console.log(element)
+// })
+
+const element = document.getElementsByName("my-input")
+console.log(element)
